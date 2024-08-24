006b3d170a78c30307e36d896819a69943985de6
Benoit-Hayet <hayetbenoit@gmail.com>
2024-08-24 12:16:48 +0200

    Get and post Ok

--- src/main/java/com/thehorrordatabase/The/Horror/Database/controller/MovieController.java
+++ src/main/java/com/thehorrordatabase/The/Horror/Database/controller/MovieController.java
@@ -2,13 +2,15 @@ package com.thehorrordatabase.The.Horror.Database.controller;
 
 import com.thehorrordatabase.The.Horror.Database.model.Movie;
 import com.thehorrordatabase.The.Horror.Database.repository.MovieRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -21,11 +23,27 @@ public class MovieController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMoviess() {
+    public ResponseEntity<List<Movie>> getAllMovies() {
         List<Movie> articles = movieRepository.findAll();
         if (articles.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie>getMovieById(@PathVariable Long id) {
+Optional<Movie> optionalMovie = movieRepository.findById(id);
+if (!optionalMovie.isPresent()) {
+    return ResponseEntity.notFound().build();
+}
+return ResponseEntity.ok(optionalMovie.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        movie.setCreatedAt(LocalDateTime.now());
+       Movie savedMovie = movieRepository.save(movie);
+       return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
     }
 }
